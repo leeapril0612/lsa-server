ec92becbbe868ff5224339d3b1590d801a3b8ff6
dongju <leeapril0612@openobject.net>
2022-02-09 16:40:17 +0900

    Update getUsers method routing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -46,8 +46,8 @@ export class UserController {
   }
 
 
-  
-  @Get('users')
+  @UseGuards(AuthGuard('jwt'))
+  @Get('list')
   public async getUsers(@Res({ passthrough: true }) response, @Request() req): Promise<Response> {
     const users = await this.userService.getUsers()
     return new ResponseMessage().success().body(users).build();
