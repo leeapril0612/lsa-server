956fc10ab1fe9e0a479b9e887e10573dd2aaeea1
leeapril0612 <leeapril0612@openobject.net>
2022-02-11 17:37:34 +0900

    Fix a cros error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  });
   await app.listen(3080);
 }
 bootstrap();
