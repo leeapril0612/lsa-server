451a4e8beb731e64c379fad54de34477fffc36ed
leeapril0612 <leeapril0612@openobject.net>
2022-02-24 12:06:33 +0900

    Update https

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import https from 'https';
+import {readFileSync} from 'fs';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  
+  const httpsOptions = {
+    key: readFileSync('./cert/localhost.dev.key'),
+    cert: readFileSync('./cert/localhost.dev.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: httpsOptions
+  });
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
@@ -12,3 +21,4 @@ async function bootstrap() {
   await app.listen(3080);
 }
 bootstrap();
+
